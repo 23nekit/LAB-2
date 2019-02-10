a5125321a91340a226b1807c0a3a54685cdac746
23nekit <23nekit@gmail.com>
2019-02-10 19:48:55 +0200

    Завдання 3 3

--- Program.cs
+++ Program.cs
@@ -2,70 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleAppProgram6
+namespace ConsoleApplication1
 {
     class Program
     {
-        static int[,] ArrayRandom(int[,] array)
+        static int[] ArrayRandom(int n)
         {
             Console.Write("Дiапазон рандому вiд :");
             int from = Convert.ToInt32(Console.ReadLine());
             Console.Write("Дiапазон рандому до :");
             int to = Convert.ToInt32(Console.ReadLine());
+            int[] arrayrandom = new int[n];
             Random RndGen = new Random();
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < arrayrandom.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1);j++)
-                {
-                    array[i,j] = RndGen.Next(from, to + 1);
-                }
+                arrayrandom[i] = RndGen.Next(from, to + 1);
             }
-            return array;
+            return arrayrandom;
         }
         static void Main(string[] args)
         {
-            Console.Write("Введiть кiлькiсть рядкiв :");
-            int nrows = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введiть кiлькiсть стовпчикiв :");
-            int ncolumns = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[nrows, ncolumns];
-            bool nozero = true;
-            int c = 0;
-            int product = 1;
-            array = ArrayRandom(array);
-            Console.WriteLine("Ваш масив :");
-            for(int i = 0; i < array.GetLength(0); i++)
+            Console.Write("Введіть кількість елементів масиву :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = ArrayRandom(n);
+            Console.WriteLine("Ваш масив :" + string.Join(" ", array));
+            Console.Write("Введіть ключ :");
+            int key = Convert.ToInt32(Console.ReadLine());
+            int ikey = -1;
+            for (int i = 0; i < array.Length; i++) 
             {
-                for(int j = 0; j < array.GetLength(1); j++)
+                if (array[i] == key) 
                 {
-                    Console.Write(" {0,3}", array[i, j]);
+                    ikey = i;
+                    break;
                 }
-                Console.WriteLine();
             }
-            for(int i = 0; i < array.GetLength(1); i++)
+            if(ikey!=-1)
             {
-                for(int j = 0; j < array.GetLength(0); j++)
+                for (int i = ikey; i < array.Length-1; i++) 
                 {
-                    if (array[j, i] == 0)
-                    {
-                        nozero = false;
-                        break;
-                    }
+                    array[i] = array[i + 1];
                 }
-                if (nozero)
-                {
-                    for(int j = 0; j < array.GetLength(0); j++)
-                    {
-                        product *= array[j, i];
-                    }
-                    Console.WriteLine("Добуток елементiв у {0} стовпчику масиву :{1}",c + 1,product);
-                    product = 1;
-                }
-                nozero = true;
-                c += 1;
+                Array.Resize(ref array, array.Length-1 );
             }
+            Console.WriteLine("Ваш масив :" + string.Join(" ", array));
             Console.ReadKey();
         }
     }
