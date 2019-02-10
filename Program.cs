73a1fe10b721009d345cc3e0ee2e95d337ecfd6a
23nekit <23nekit@gmail.com>
2019-02-10 21:50:42 +0200

    Завдання 4 9

--- Program.cs
+++ Program.cs
@@ -8,49 +8,71 @@ namespace ConsoleAppProgram6
 {
     class Program
     {
-        static int[,] ArrayRandom(int[,] array)
+        static int[][] ArrayRandom(int[][] array)
         {
             Console.Write("Дiапазон рандому вiд :");
             int from = Convert.ToInt32(Console.ReadLine());
             Console.Write("Дiапазон рандому до :");
             int to = Convert.ToInt32(Console.ReadLine());
             Random RndGen = new Random();
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1);j++)
+                for (int j = 0; j < array[0].Length; j++)
                 {
-                    array[i,j] = RndGen.Next(from, to + 1);
+                    array[i][j] = RndGen.Next(from, to + 1);
                 }
             }
             return array;
         }
         static void Main(string[] args)
         {
-            Console.Write("Введiть кiлькiсть рядкiв та стовпчикiв :");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[n, n];
+            Console.Write("Введiть кiлькiсть рядкiв :");
+            int nrows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введiть кiлькiсть стовпчикiв :");
+            int ncolumns = Convert.ToInt32(Console.ReadLine());
+            int[][] array = new int[nrows][];
+            int maxelement;
+            int imaxelement;
+            for(int i = 0; i < nrows; i++)
+            {
+                array[i] = new int[ncolumns];
+            }
             array = ArrayRandom(array);
             Console.WriteLine("Початковий масив :");
-            for(int i = 0; i < array.GetLength(0); i++)
+            for(int i = 0; i < array.Length; i++)
             {
-                for(int j = 0; j < array.GetLength(1); j++)
+                for(int j = 0; j < array[0].Length; j++)
                 {
-                    Console.Write(" {0,3}", array[i, j]);
+                    Console.Write(" {0,3}", array[i][j]);
                 }
                 Console.WriteLine();
             }
-            for(int i = 0; i < array.GetLength(0); i++)
+            maxelement = array[0][0];
+            imaxelement = 0;
+            for(int i = 0; i < array.Length; i++)
             {
-                int t = array[i,i];
-                array[i, i] = array[i, array.GetLength(0) - 1 - i];
-                array[i, array.GetLength(0) - 1 - i] = t;
-            }
-            Console.WriteLine("Перетворений масив :");
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < array[i].Length; j++)
                 {
-                    Console.Write(" {0,3}", array[i, j]);
+                    if (array[i][j] > maxelement)
+                    {
+                        maxelement = array[i][j];
+                        imaxelement = i;
+                    }
+                }
+            }
+            for (int i = imaxelement; i < array.Length - 1; i++) 
+            {
+                int[] t = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = t;
+            }
+            Array.Resize(ref array, array.Length - 1);
+            Console.WriteLine("Перетворений масив :");
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[0].Length; j++)
+                {
+                    Console.Write(" {0,3}", array[i][j]);
                 }
                 Console.WriteLine();
             }
