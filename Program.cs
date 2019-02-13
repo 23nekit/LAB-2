73c63a3a1ca8c92271b5ae6765131612bcb0cf79
23nekit <23nekit@gmail.com>
2019-02-14 01:03:15 +0200

    2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,45 @@ namespace ConsoleAppProgram6
 {
     class Program
     {
+        static void ArrayWrite(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(" {0,3}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void ArrayEdit(int[,] array)
+        {
+            bool nozero = true;
+            int c = 0;
+            int product = 1;
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    if (array[j, i] == 0)
+                    {
+                        nozero = false;
+                        break;
+                    }
+                }
+                if (nozero)
+                {
+                    for (int j = 0; j < array.GetLength(0); j++)
+                    {
+                        product *= array[j, i];
+                    }
+                    Console.WriteLine("Добуток елементiв у {0} стовпчику масиву :{1}", c + 1, product);
+                    product = 1;
+                }
+                nozero = true;
+                c += 1;
+            }
+        }
         static int[,] ArrayRandom(int[,] array)
         {
             Console.Write("Дiапазон рандому вiд :");
@@ -17,9 +56,9 @@ namespace ConsoleAppProgram6
             Random RndGen = new Random();
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1);j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
-                    array[i,j] = RndGen.Next(from, to + 1);
+                    array[i, j] = RndGen.Next(from, to + 1);
                 }
             }
             return array;
@@ -31,41 +70,10 @@ namespace ConsoleAppProgram6
             Console.Write("Введiть кiлькiсть стовпчикiв :");
             int ncolumns = Convert.ToInt32(Console.ReadLine());
             int[,] array = new int[nrows, ncolumns];
-            bool nozero = true;
-            int c = 0;
-            int product = 1;
             array = ArrayRandom(array);
             Console.WriteLine("Ваш масив :");
-            for(int i = 0; i < array.GetLength(0); i++)
-            {
-                for(int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(" {0,3}", array[i, j]);
-                }
-                Console.WriteLine();
-            }
-            for(int i = 0; i < array.GetLength(1); i++)
-            {
-                for(int j = 0; j < array.GetLength(0); j++)
-                {
-                    if (array[j, i] == 0)
-                    {
-                        nozero = false;
-                        break;
-                    }
-                }
-                if (nozero)
-                {
-                    for(int j = 0; j < array.GetLength(0); j++)
-                    {
-                        product *= array[j, i];
-                    }
-                    Console.WriteLine("Добуток елементiв у {0} стовпчику масиву :{1}",c + 1,product);
-                    product = 1;
-                }
-                nozero = true;
-                c += 1;
-            }
+            ArrayWrite(array);
+            ArrayEdit(array);
             Console.ReadKey();
         }
     }
