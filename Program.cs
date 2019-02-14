3e8ae75823c03524f7e43ae4fcce1fcf43127693
23nekit <23nekit@gmail.com>
2019-02-14 02:03:34 +0200

    111

--- Program.cs
+++ Program.cs
@@ -2,58 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleAppProgram6
+namespace ConsoleApplication1
 {
     class Program
     {
-        static void ArrayWrite(int[,] array)
+        static void ArrayWrite(int[] array)
         {
-            for (int i = 0; i < array.GetLength(0); i++)
+            Console.WriteLine("Ваш масив :" + string.Join(" ", array));
+        }
+        static void ArrayEdit(int[] array)
+        {
+            Console.Write("Введіть ключ :");
+            int key = Convert.ToInt32(Console.ReadLine());
+            int ikey = -1;
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                if (array[i] == key)
                 {
-                    Console.Write(" {0,3}", array[i, j]);
+                    ikey = i;
+                    break;
                 }
-                Console.WriteLine();
             }
-        }
-        static void ArrayEdit(int[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
+            if (ikey != -1)
             {
-                int t = array[i, i];
-                array[i, i] = array[i, array.GetLength(0) - 1 - i];
-                array[i, array.GetLength(0) - 1 - i] = t;
+                for (int i = ikey; i < array.Length - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                Array.Resize(ref array, array.Length - 1);
             }
         }
-        static int[,] ArrayRandom(int[,] array)
+        static int[] ArrayRandom(int n)
         {
             Console.Write("Дiапазон рандому вiд :");
             int from = Convert.ToInt32(Console.ReadLine());
             Console.Write("Дiапазон рандому до :");
             int to = Convert.ToInt32(Console.ReadLine());
+            int[] arrayrandom = new int[n];
             Random RndGen = new Random();
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < arrayrandom.Length; i++)
             {
-                for (int j = 0; j < array.GetLength(1);j++)
-                {
-                    array[i,j] = RndGen.Next(from, to + 1);
-                }
+                arrayrandom[i] = RndGen.Next(from, to + 1);
             }
-            return array;
+            return arrayrandom;
         }
         static void Main(string[] args)
         {
-            Console.Write("Введiть кiлькiсть рядкiв та стовпчикiв :");
+            Console.Write("Введіть кількість елементів масиву :");
             int n = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[n, n];
-            array = ArrayRandom(array);
-            Console.WriteLine("Початковий масив :");
+            int[] array = ArrayRandom(n);
             ArrayWrite(array);
             ArrayEdit(array);
-            Console.WriteLine("Перетворений масив :");
             ArrayWrite(array);
             Console.ReadKey();
         }
